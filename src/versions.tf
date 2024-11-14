37762e389fb77e124ada36a665003cc1877896ed
Igor Rodionov <goruha@gmail.com>
2024-11-14 20:22:54 +0100

    Initial commit

--- src/versions.tf
+++ src/versions.tf
@@ -1,5 +1,14 @@
 terraform {
   required_version = ">= 1.0.0"
 
-  required_providers {}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = ">= 4.0"
+    }
+  }
 }
